88b09f0e01a9734c4a3a2d4c5b9f1556c0f4916a
Ibraheem AbdulRahman <ibraheemabdulrahman305@gmail.com>
2026-06-02 15:42:10 -0700

    feat: add Pacifico font styling and update layout in App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,16 @@ export function App() {
   return (
     <>
       <div className="mx-auto min-h-screen items-center justify-center p-4">
-        <div className="flex max-w-full flex-col gap-4 text-sm">
-          <div className="flex flex-row items-center justify-center gap-4">
-            <NavigationMenu />
+          <div className="p-5">
+            <h1 className="text-2xl text-light pacifico-regular">Dribbles</h1>
+            <div className="flex flex-row items-center justify-center gap-4 -mt-8">
+              <NavigationMenu />
+            </div>
           </div>
           <ButtonComponent />
           <div className="font-mono text-xs text-muted-foreground">
             (Press <kbd>d</kbd> to toggle dark mode)
           </div>
-        </div>
       </div>
     </>
   )
