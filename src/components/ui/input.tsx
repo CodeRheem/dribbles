2189e94d61f6e06165fe90185a44767a2a982240
Ibraheem AbdulRahman <ibraheemabdulrahman305@gmail.com>
2026-06-03 14:42:06 -0700

    feat: add search icon to Button in Input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -37,7 +37,7 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenuRoot>
-        <Button className={"bg-pink-400 border rounded-4xl"}></Button>
+        <Button className={"bg-pink-400 border rounded-4xl hover:bg-pink-300"}><svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-search-icon lucide-search"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg></Button>
       </div>
     </div>
   )
