d761d9ea3973cd7863b9d467b34922c57192e2cb
Ibraheem AbdulRahman <ibraheemabdulrahman305@gmail.com>
2026-06-21 01:10:07 -0700

    Add new image: reet-talreja-7Ne_rNvQldw-unsplash.jpg to public directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,33 @@
 import NavigationMenu from "./components/navmenu"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+
 
 export function App() {
   return (
-    <div className="mx-auto min-h-screen p-4">
+    <div 
+      className="mx-auto min-h-screen p-4 bg-cover bg-center rounded-sm border-10 border-white"
+      style={{ backgroundImage: "url('/oscar-nord--U4GwXGP5ew-unsplash.jpg')" }}
+    >
 
       {/* Navbar */}
-      <header className="flex items-center gap-6 px-5 py-4">
-        <h1 className="text-2xl text-light pacifico-regular shrink-0">Dribbles</h1>
-
-        <div className="flex-1 max-w-lg">
-          <Input placeholder="What are you looking for?" />
+      <header className="relative z-10 flex justify-center items-center px-5 py-4 text-white dark:text-white">
+        <div className="absolute left-35">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-circle-arrow-out-up-left-icon lucide-circle-arrow-out-up-left"><path d="M2 8V2h6"/><path d="m2 2 10 10"/><path d="M12 2A10 10 0 1 1 2 12"/></svg>
         </div>
-
-        <nav className="pl-4">
+        <nav>
           <NavigationMenu />
         </nav>
-
-        <div className="absolute right-15 flex items-center gap-2">
-          <Button variant="outline" className="border rounded-4xl p-6">Sign up</Button>
-          <Button  className="border rounded-4xl p-6">Log in</Button>
+        <div className="absolute right-35 flex items-center gap-4">
+          <span>Buy Now</span>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-tally1-icon lucide-tally-1"><path d="M4 4v16"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-menu-icon lucide-menu"><path d="M4 5h16"/><path d="M4 12h16"/><path d="M4 19h16"/></svg>
         </div>
       </header>
 
       {/* Page content */}
-      <main className="flex flex-col items-center justify-center gap-4 p-4">
-        <div>
-          <div>
-            <h2>Smart Home Landing Page Design</h2>
-          </div>
-        </div>
-        <p className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </p>
-      </main>
-
+      <main className="relative z-0 flex flex-col items-center justify-center text-white">
+        <h1 className="text-4xl font-bold mb-4">Elevate Your Living Experience</h1>
+        <p className="text-lg">This is a sample page content.</p>
+      </main>   
     </div>
   )
 }
--- src/components/navmenu.tsx
+++ src/components/navmenu.tsx
@@ -10,32 +10,26 @@ import {
 export default function NavigationMenu() {
   return (
     <NavigationMenuRoot >
-      <NavigationMenuList>
+      <NavigationMenuList className="gap-9">
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Explore</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink className={"font-bold"}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-trending-up-icon lucide-trending-up"><path d="M16 7h6v6"/><path d="m22 7-8.5 8.5-5-5L2 17"/></svg>Popular</NavigationMenuLink>
-            <NavigationMenuLink className={"font-bold"}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-sparkles-icon lucide-sparkles"><path d="M11.017 2.814a1 1 0 0 1 1.966 0l1.051 5.558a2 2 0 0 0 1.594 1.594l5.558 1.051a1 1 0 0 1 0 1.966l-5.558 1.051a2 2 0 0 0-1.594 1.594l-1.051 5.558a1 1 0 0 1-1.966 0l-1.051-5.558a2 2 0 0 0-1.594-1.594l-5.558-1.051a1 1 0 0 1 0-1.966l5.558-1.051a2 2 0 0 0 1.594-1.594z"/><path xmlns="M20 2v4"/><path d="M22 4h-4"/><circle cx="4" cy="20" r="2"/></svg>New and Noteworthy</NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <hr />
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Hire Talent</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-          </NavigationMenuContent>
+          <NavigationMenuLink>
+            <a href="/about">About</a>
+          </NavigationMenuLink>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Get Hired</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-          </NavigationMenuContent>
+          <NavigationMenuLink>
+            <a href="/products">Products</a>
+          </NavigationMenuLink>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Community</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-          </NavigationMenuContent>
+          <NavigationMenuLink>
+            <a href="/features">Features</a>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink>
+            <a href="/contacts">Contacts</a>
+          </NavigationMenuLink>
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenuRoot>
